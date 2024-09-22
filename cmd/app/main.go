727b3b49514a01e068438540474e8c971e2dfd9f
Ovchinnikov Alexey <gdhald4@gmail.com>
2024-09-22 21:32:27 +0500

    add feature

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Hell077/GoBoost/internal/goBoostBuild"
 	"github.com/Hell077/GoBoost/internal/goBoostStruct"
 	"github.com/Hell077/GoBoost/internal/handlers"
@@ -10,27 +11,35 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	if len(args) > 0 {
-		switch strings.ToLower(args[0]) {
-		case "--help", "-h":
-			handlers.ShowHelp()
-			return
-		case "--update":
-			handlers.UpdateProgram()
-			return
-		case "--build":
-			goBoostBuild.Build()
-			return
-		case "--struct":
-			goBoostStruct.Structure()
-			return
-		default:
-			if args[0] == "create" {
-				handlers.CreateProject()
-				return
-			}
-		}
+
+	if len(args) == 0 {
+		handlers.CreateProject()
+		return
 	}
 
-	handlers.CreateProject()
+	if len(args[0]) == 0 {
+		fmt.Println("Error: Empty flag detected. Please provide a valid flag.")
+		return
+	}
+
+	switch strings.ToLower(args[0]) {
+	case "--help", "--h":
+		handlers.ShowHelp()
+		return
+	case "--update", "--u":
+		handlers.UpdateProgram()
+		return
+	case "--build", "--b":
+		goBoostBuild.Build()
+		return
+	case "--struct", "--s":
+		goBoostStruct.Structure()
+		return
+	case "create":
+		handlers.CreateProject()
+		return
+	default:
+		fmt.Printf("Error: Invalid flag '%s'. Use --help or -h for available commands.\n", args[0])
+		return
+	}
 }
